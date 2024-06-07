223972869a924edd616c37425023792f3b297bd1
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-06-07 16:04:40 +0200

    6/7/2024

--- src/components/formulaire/info.tsx
+++ src/components/formulaire/info.tsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
-import './info.css'
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import './info.css';
 
-function Information({ onClose }) {
-  const [formData, setFormData] = useState({
+interface InformationProps {
+  onClose: () => void;
+}
+
+interface FormData {
+  deliveryOption: string;
+  fullNameOrCompany: string;
+  email: string;
+  phoneNumber: string;
+  postalCode: string;
+  city: string;
+  deliveryAddress: string;
+}
+
+const Information: React.FC<InformationProps> = ({ onClose }) => {
+  const [formData, setFormData] = useState<FormData>({
     deliveryOption: '',
     fullNameOrCompany: '',
     email: '',
@@ -10,24 +24,24 @@ function Information({ onClose }) {
     postalCode: '',
     city: '',
     deliveryAddress: ''
-  })
+  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
     setFormData((prevState) => ({
       ...prevState,
       [name]: value
-    }))
-  }
+    }));
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(formData)
-  }
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   const handleClose = () => {
-    if (onClose) onClose() // Assuming onClose toggles the visibility of the Information component
-  }
+    if (onClose) onClose(); // Assuming onClose toggles the visibility of the Information component
+  };
 
   return (
     <form className="formeBox" onSubmit={handleSubmit}>
@@ -38,21 +52,48 @@ function Information({ onClose }) {
       </div>
       <div className="section secondSection">
         <label htmlFor="fullNameOrCompany">Nom complet ou Société</label>
-        <input className="label-class" type="text" id="fullNameOrCompany" name="fullNameOrCompany" value={formData.fullNameOrCompany} onChange={handleChange} />
+        <input
+          className="label-class"
+          type="text"
+          id="fullNameOrCompany"
+          name="fullNameOrCompany"
+          value={formData.fullNameOrCompany}
+          onChange={handleChange}
+        />
       </div>
       <div className="secondSection">
         <label htmlFor="email">Email</label>
-        <input className="label-class" type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+        <input
+          className="label-class"
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
       </div>
       <div className="secondSection">
         <label htmlFor="phoneNumber">Numéro de téléphone</label>
-        <input className="label-class" type="tel" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+        <input
+          className="label-class"
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+        />
       </div>
       <div className="Optiondelivraison secondSection">
         <label htmlFor="deliveryOption" className="label-deliveryOption">
           Option de livraison
         </label>
-        <select id="deliveryOption" name="deliveryOption" value={formData.deliveryOption} className="delivery-option-select" onChange={handleChange}>
+        <select
+          id="deliveryOption"
+          name="deliveryOption"
+          value={formData.deliveryOption}
+          className="delivery-option-select"
+          onChange={handleChange}
+        >
           <option value="storePickup">Retrait du magasin</option>
           <option value="homeDelivery">Livraison à domicile</option>
         </select>
@@ -61,15 +102,36 @@ function Information({ onClose }) {
         <>
           <div className="secondSection">
             <label htmlFor="deliveryAddress">Adresse de livraison</label>
-            <input className="label-class" type="text" id="deliveryAddress" name="deliveryAddress" value={formData.deliveryAddress} onChange={handleChange} />
+            <input
+              className="label-class"
+              type="text"
+              id="deliveryAddress"
+              name="deliveryAddress"
+              value={formData.deliveryAddress}
+              onChange={handleChange}
+            />
           </div>
           <div className="secondSection">
             <label htmlFor="city">Ville</label>
-            <input className="label-class" type="text" id="city" name="city" value={formData.city} onChange={handleChange} />
+            <input
+              className="label-class"
+              type="text"
+              id="city"
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+            />
           </div>
           <div className="secondSection">
             <label htmlFor="postalCode">Code Postal</label>
-            <input className="label-class" type="text" id="postalCode" name="postalCode" value={formData.postalCode} onChange={handleChange} />
+            <input
+              className="label-class"
+              type="text"
+              id="postalCode"
+              name="postalCode"
+              value={formData.postalCode}
+              onChange={handleChange}
+            />
           </div>
         </>
       )}
@@ -79,6 +141,7 @@ function Information({ onClose }) {
         </button>
       </div>
     </form>
-  )
-}
-export default Information
+  );
+};
+
+export default Information;
