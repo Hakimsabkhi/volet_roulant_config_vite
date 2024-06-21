bd9bee4cb6333bece5504da992ba57bce52533d4
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-06-21 13:21:13 +0200

    fix phone layout

--- src/components/steps/couleurVolet.tsx
+++ src/components/steps/couleurVolet.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setColor } from '../../features/voletSlice'
 import './couleurVolet.css'
 import { ColorImages } from '../../assets/Data'
 import { RootState } from '../../store'; // Assuming you have a RootState type defined in your store file
+import useMediaQuery from './useMediaQuery'; // Adjust the path as necessary
 
 interface SelectedColor {
   coulisse: string;
@@ -18,16 +19,22 @@ interface CouleurVoletProps {
 const CouleurVolet: React.FC<CouleurVoletProps> = ({ enableNextButton }) => {
   const dispatch = useDispatch();
   const selectedColors: SelectedColor = useSelector((state: RootState) => state.volet.selectedColor);
+  const [currentSection, setCurrentSection] = useState<keyof SelectedColor>('coulisse');
+  const isSmallScreen = useMediaQuery('(max-width: 1050px)');
 
   useEffect(() => {
     const allSelected = Object.keys(ColorImages).every(
       (category) => selectedColors[category as keyof SelectedColor] && selectedColors[category as keyof SelectedColor] !== ''
     );
     enableNextButton(allSelected);
-  }, [selectedColors, enableNextButton]); // Temporarily remove enableNextButton
+  }, [selectedColors, enableNextButton]);
 
   const handleColorSelection = (colorName: string, category: keyof SelectedColor) => {
     dispatch(setColor({ color: colorName, category }));
+    if (isSmallScreen) {
+      if (category === 'coulisse') setCurrentSection('tablier');
+      if (category === 'tablier') setCurrentSection('lameFinale');
+    }
   };
 
   const renderColorChoices = (category: keyof SelectedColor) => {
@@ -59,18 +66,24 @@ const CouleurVolet: React.FC<CouleurVoletProps> = ({ enableNextButton }) => {
 
   return (
     <div className="ColorBox">
-      <div>
-        <h2>Coffre et Coulisse</h2>
-        <div className="colors-row">{renderColorChoices('coulisse')}</div>
-      </div>
-      <div>
-        <h2>Tablier</h2>
-        <div className="colors-row">{renderColorChoices('tablier')}</div>
-      </div>
-      <div>
-        <h2>Lame Finale</h2>
-        <div className="colors-row">{renderColorChoices('lameFinale')}</div>
-      </div>
+      {(!isSmallScreen || currentSection === 'coulisse') && (
+        <div>
+          <h2>Coffre et Coulisse</h2>
+          <div className="colors-row">{renderColorChoices('coulisse')}</div>
+        </div>
+      )}
+      {(!isSmallScreen || currentSection === 'tablier') && (
+        <div>
+          <h2>Tablier</h2>
+          <div className="colors-row">{renderColorChoices('tablier')}</div>
+        </div>
+      )}
+      {(!isSmallScreen || currentSection === 'lameFinale') && (
+        <div>
+          <h2>Lame Finale</h2>
+          <div className="colors-row">{renderColorChoices('lameFinale')}</div>
+        </div>
+      )}
     </div>
   );
 };
